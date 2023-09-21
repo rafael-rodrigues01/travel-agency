9a10b975ef70a6779ce731267d390cfd0482744a
Rafael <rafa_rodrigues01@yahoo.com>
2023-09-20 21:45:45 -0300

    imported: GlobalStyle component

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import * as S from './styles/global.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <S.GlobalStyle />
     <App />
   </React.StrictMode>,
 )
